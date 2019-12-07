0b7c3bc5160b897786da0c948a7c0ef60913003d
Edoardo Facchinelli <silpheel@gmail.com>
2019-12-07 18:19:18 -0500

    Fix for files not being imported.
    Workaround for profile not loading automatically.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 # Profile for the Microsoft.Powershell Shell, only. (Not Visual Studio or other PoSh instances)
 # ===========
-
+echo "Booting MS profile"
 Push-Location (Split-Path -parent $profile)
-"components-shell" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
+"components-shell","components","functions","aliases","exports","extra" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
